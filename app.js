a8f59129d18898cca8b3aec86dfd23eea750c9b0
Talha Godil <mtajig05@gmail.com>
2021-05-03 12:19:39 +0500

    ready to commit

---
+++ app.js
@@ -0,0 +1,114 @@
+// CHAPTER # 43 - 48
+
+// 1. Show an alert box on click on a link. 
+
+// function message (){
+//     alert("This site is under construction")
+// }
+
+// 2. Display some Mobile images in browser. On click on an 
+// image Show the message in alert to user.
+
+// function message1(){
+//     alert("Thanks for choosing our store...!")
+// }
+
+// 3. Display 10 student records in table and each row should contain a delete 
+// button. If you click on a button to delete a record, entire row should be 
+// deleted. 
+
+// function clearBtn (){
+//   var clear = document.getElementById("clear")
+//   clear.innerHTML = ""
+//    var clear1 = document.getElementById("clear1")
+//    clear1.innerHTML = " "
+//   var clear2 = document.getElementById("clear2")
+//   clear2.innerHTML = " "
+//   var clear3 = document.getElementById("clear3")
+//    clear3.innerHTML = " "
+//    var clear4 = document.getElementById("clear4")
+//    clear4.innerHTML = " "
+//    var clear5 = document.getElementById("clear5")
+//   clear5.innerHTML = " "
+//   var clear6 = document.getElementById("clear6")
+//    clear6.innerHTML = " "
+   
+// }
+
+
+// function changePicture (){
+//  var anyPicture = document.getElementById("neymar")
+//  anyPicture.src="https://fsa.zobj.net/crop.php?r=Df-lbfYa-CpfcUSIhW5pKuVKcG0lS-wDA4W6D-6A7vfSWdMaSJHng4td-jBO6XBGihAaIx3TrPlX5HMmLq16SRYwkSKupk0YNKji-15P7MBoxiPCtVQBpufheufCbTdPE8ufzvD5tLMJOxDk"
+// }
+
+//function changePictureAfter (){
+  //var anyPicture = document.getElementById("neymar")
+  //anyPicture.src="images/neymar.jpg"
+//}
+
+
+//function input1(){
+ //var add = document.getElementById("add")
+ //add.value += +1
+ //var sub = document.getElementById("sub")
+ //sub.value += -1
+ //console.log(sub)
+//}
+  
+//CHAPTER 49 -52
+
+//function login (){
+ // var fullName = document.getElementById("fullname")
+//var a = fullName.value;
+//document.write("Full Name : " + " " + a)
+//var email = document.getElementById("email")
+//var b = email.value;
+//document.write("Email Address :" + " " + b)
+//var number = document.getElementById("number")
+//var c = number.value;
+//document.write ("Phone Number : " + " " + c)
+// }
+
+//function readMore(){
+ // var text = "The original console in the series was the first console of any type to ship over 100 million units, doing so in under a decade.[2] Its successor, the PlayStation 2, was released in 2000. The PlayStation 2 is the best-selling home console to date, having reached over 155 million units sold by the end of 2012.[3] Sony's next console, the PlayStation 3, was released in 2006, selling over 87.4 million units by March 2017.[4] Sony's latest console, the PlayStation 4, was released in 2013, selling a million units within a day, becoming the fastest selling console in history.[5] The next console in the series, the PlayStation 5, is expected to be released by the end of 2020.[6]total of 80 million units worldwide by November 2013.[7] Its successor, the PlayStation Vita, which launched in Japan in December 2011 and in most other major territories in February 2012, selling over four million units by January 2013.[8] PlayStation TV is a microconsole and a non-portable variant of the PlayStation Vita handheld game console.[9] Other hardware released as part of the PlayStation series includes the PSX, a digital video recorder which was integrated with the PlayStation and PlayStation 2, though it was short lived due to its high price and was never released outside Japan, as well as a Sony Bravia television set which has an integrated PlayStation 2. The main series of controllers utilized by the PlayStation series is the DualShock, which is a line of vibration-feedback gamepad having sold 28 million controllers by June 2008.[10]"
+ // var pera1 = document.getElementById("pera1")
+ // pera1.innerHTML = text
+  
+//}
+//function readMore1(){
+ // var text = "The original console in the series was the first console of any type to ship over 100 million units, doing so in under a decade.[2] Its successor, the PlayStation 2, was released in 2000. The PlayStation 2 is the best-selling home console to date, having reached over 155 million units sold by the end of 2012.[3] Sony's next console, the PlayStation 3, was released in 2006, selling over 87.4 million units by March 2017.[4] Sony's latest console, the PlayStation 4, was released in 2013, selling a million units within a day, becoming the fastest selling console in history.[5] The next console in the series, the PlayStation 5, is expected to be released by the end of 2020.[6]total of 80 million units worldwide by November 2013.[7] Its successor, the PlayStation Vita, which launched in Japan in December 2011 and in most other major territories in February 2012, selling over four million units by January 2013.[8] PlayStation TV is a microconsole and a non-portable variant of the PlayStation Vita handheld game console.[9] Other hardware released as part of the PlayStation series includes the PSX, a digital video recorder which was integrated with the PlayStation and PlayStation 2, though it was short lived due to its high price and was never released outside Japan, as well as a Sony Bravia television set which has an integrated PlayStation 2. The main series of controllers utilized by the PlayStation series is the DualShock, which is a line of vibration-feedback gamepad having sold 28 million controllers by June 2008.[10]"
+ // var  pera2 = document.getElementById("pera2")
+ // pera2.innerHTML = text
+//}
+///function readMore2(){
+ // var text = "The original console in the series was the first console of any type to ship over 100 million units, doing so in under a decade.[2] Its successor, the PlayStation 2, was released in 2000. The PlayStation 2 is the best-selling home console to date, having reached over 155 million units sold by the end of 2012.[3] Sony's next console, the PlayStation 3, was released in 2006, selling over 87.4 million units by March 2017.[4] Sony's latest console, the PlayStation 4, was released in 2013, selling a million units within a day, becoming the fastest selling console in history.[5] The next console in the series, the PlayStation 5, is expected to be released by the end of 2020.[6]total of 80 million units worldwide by November 2013.[7] Its successor, the PlayStation Vita, which launched in Japan in December 2011 and in most other major territories in February 2012, selling over four million units by January 2013.[8] PlayStation TV is a microconsole and a non-portable variant of the PlayStation Vita handheld game console.[9] Other hardware released as part of the PlayStation series includes the PSX, a digital video recorder which was integrated with the PlayStation and PlayStation 2, though it was short lived due to its high price and was never released outside Japan, as well as a Sony Bravia television set which has an integrated PlayStation 2. The main series of controllers utilized by the PlayStation series is the DualShock, which is a line of vibration-feedback gamepad having sold 28 million controllers by June 2008.[10]"
+ // var  pera3 = document.getElementById("pera3")
+ // pera3.innerHTML = text
+//}
+
+  
+//CHAPTER 58 - 67
+
+//var content = document.getElementById("main-content")
+//var a = content
+
+
+//console.log(document.childNodes[1].childNodes[1].childNodes[14].childNodes[5])
+
+//var render = document.getElementsByClassName("”render”")
+//render = render.innerHTML
+
+//var text = document.createTextNode("Talha Godil")
+//input.appendChild (text)
+//var firstname = document.getElementById("”first-name”")
+//firstname.appendChild(text)
+//comsole.log (firstname)
+
+//CHAPTER 52 - 57
+
+//function modal(e){
+ // console.log(e.src)
+  //var modal = document.getElementById("modalimage")
+ // modal.src = e.src
+ 
+//}
